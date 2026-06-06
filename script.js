38e06e1410e07c612f0a5f27da1b505661a4fb5c
KhoaMinh <93652831+KhoaMinhPMK@users.noreply.github.com>
2026-06-06 16:46:02 +0700

    Replace all PDFs and MP4s in galleries and mediaMap with image URLs

--- script.js
+++ script.js
@@ -127,8 +127,8 @@ const mediaLibrary = [
     title: "Cont-Tect Motion Masters",
     url: "./Drive%20For%20Portfolio%20Website/5T_3.9/5T.jpeg",
     gallery: [
-      "./Drive%20For%20Portfolio%20Website/CONT-TECT_-_Motion_Masters/MOTION_MASTERS.mp4",
-      "./Drive%20For%20Portfolio%20Website/CONT-TECT_-_Motion_Masters/MOTION_MASTERS___FINALE_ROUND___CONTTECT.pdf",
+      "./Drive%20For%20Portfolio%20Website/5T_3.9/5T.jpeg",
+      "./Drive%20For%20Portfolio%20Website/5T_3.9/5T.jpeg",
     ],
   },
   {
@@ -169,7 +169,7 @@ const mediaLibrary = [
     title: "Academic evidence",
     url: "./Drive%20For%20Portfolio%20Website/5T_3.9/5T.jpeg",
     gallery: [
-      "./Drive%20For%20Portfolio%20Website/5T_3.9/5T.mp4",
+      "./Drive%20For%20Portfolio%20Website/5T_3.9/5T.jpeg",
       "./Drive%20For%20Portfolio%20Website/5T_3.9/GPA.jpg",
     ],
   },
